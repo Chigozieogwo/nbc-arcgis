71cd2c079be7f5034a2cbd19e14fe0a237555255
chigozie <ogwoekechimic@gmail.com>
2023-03-28 22:15:55 +0100

    first commit2

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -22,12 +22,12 @@ const LoginScreen = () => {
 
   const redirect = location.search
      ? location.search.split('=')[1]
-     : '/';
+     : '/map';
 
   useEffect(() => {
-     if (userInfo) {
-        navigate(redirect);
-     }
+    //  if (userInfo) {
+    //     navigate(redirect);
+    //  }
   }, [navigate, userInfo, redirect]);
 
   const submitHandler = (e) => {
@@ -86,10 +86,10 @@ const LoginScreen = () => {
                   </label> */}
                 </div>
                 <div class="form-control mt-6">
-                <Link >
+                <Link  to="/map">
                   <button
                       type="button"
-                      onClick={submitHandler}
+                      
                     class="text-white w-full  bg-green-700 hover:bg-green-600 focus:ring-4 focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
                    Login
                     
