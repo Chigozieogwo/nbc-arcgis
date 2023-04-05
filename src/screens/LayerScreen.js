409b5fbe6f750690a38f57f206982a4c856b0889
chigozie <ogwoekechimic@gmail.com>
2023-04-05 15:37:20 +0100

    arc

--- src/screens/LayerScreen.js
+++ src/screens/LayerScreen.js
@@ -158,7 +158,7 @@ const LayerScreen = ({ match }) => {
       dispatch(featureLayerDetailsAction());
       setGeojsonData(layer?.geometryContent);
      
-      
+
     }, []);
   
   
@@ -266,6 +266,8 @@ const LayerScreen = ({ match }) => {
                   <img className='w-7' src={file}></img>
                   <div className="flex justify-center items-center">
 
+
+
                   <p className="text-center text-sm font-medium mx-2 text-white ">New Layer</p>
                   </div>
                 </div>
@@ -273,11 +275,14 @@ const LayerScreen = ({ match }) => {
 
                 <div className="space-y-4 bg-white rounded-sm px-3 py-2 mt-4">
                 <div class="flex items-center mt-3">
-    <input id="link-checkbox" type="checkbox" value="" class="w-4 h-4 text-teal-600 bg-gray-100 border-gray-300 rounded focus:ring-teal-500 dark:focus:ring-teal-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"></input>
+    {/* <input id="link-checkbox" type="checkbox" value="" class="w-4 h-4 text-teal-600 bg-gray-100 border-gray-300 rounded focus:ring-teal-500 dark:focus:ring-teal-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"></input> */}
     <label for="link-checkbox" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Layers <a href="#" class="text-teal-600 dark:text-teal-500 hover:underline"></a></label>
 </div>
                 
                 
+
+
+
 {
     featureLayers?.map((layer, index) =>
     <div class="flex items-center ml-4">
